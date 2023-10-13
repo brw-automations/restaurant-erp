422485f7f2cf904d9dbdf359249b4edd8393ea6d
cmwema <calwbmwema65@gmail.com>
2023-10-13 14:33:02 +0300

    added a submit handler to prevent reloading

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./Search.css";
 
 function Search() {
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  }
   return (
     <div className="search__form-wrapper">
-      <form method="get" action="">
+      <form onSubmit={submitHandler}>
         <input className="search__input" type="text" placeholder="Search products " />
         <input className="search__btn" type="submit" value="search" />
       </form>
